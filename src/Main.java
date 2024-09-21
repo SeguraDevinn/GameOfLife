0c48125acff5c0ce29b3f674bb9f4a7cd7e6b7bb
devinnsegura <devinn.segura@calbaptist.edu>
2024-09-20 17:50:28 -0700

    Finished function implementation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     private static final int SIZE = 10;
     private static int[][] grid = new int[SIZE][SIZE];
@@ -12,16 +10,36 @@ public class Main {
 
     }
 
+    public static void evolve() {
+        int[][] newGrid = new int[SIZE][SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                int neighbors = countNeighbors(i,j);
+                if (grid[i][j] == 1) {
+                    newGrid[i][j] = (neighbors == 2 || neighbors == 3) ? 1:0;
+                } else {
+                    newGrid[i][j] = (neighbors == 3) ? 1 : 0;
+                }
+            }
+        }
+
+        grid = newGrid;
+    }
+
     public static int countNeighbors(int x, int y) {
         int count = 0;
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
-                if (i == 0 && j == 0) {
-
+                if (i == 0 && j == 0) continue;
+                    int nextX = x +i;
+                    int nextY = y + j;
+                    if (nextX >= 0 && nextY >= 0  && nextX < SIZE && nextY < SIZE) {
+                        count += grid[nextX][nextY];
+                    }
                 }
-            }
-
         }
+        return count;
     }
 
     private static void printGrid() {
